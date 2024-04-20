d090499306df0895095ba04ce61c2527b97d4083
francisfzg <73186609+francisfzg@users.noreply.github.com>
2024-04-20 21:54:13 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,21 @@ let mytoken= ['auto'];//快速订阅访问入口, 留空则不启动快速订阅
 let addresses = [
 	//'icook.tw:2053#资源优选域名',
 	//'cloudflare.cfgo.cc#优选官方线路',
-	    'time.cloudflare.com:2095#优质资源',
+];
+
+// 设置优选地址api接口
+let addressesapi = [
+	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesapi.txt', //可参考内容格式 自行搭建。
+	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesipv6api.txt', //IPv6优选内容格式 自行搭建。
+	'https://wenben-ehq.pages.dev/IP.txt?token=4352934',
+];
+
+// 设置优选地址，不带端口号默认80，noTLS订阅生成
+let addressesnotls = [
+	//'www.visa.com.sg#资源优选域名',
+	//'www.wto.org:8080#官方优选域名',
+	//'www.who.int:8880#官方优选域名',
+	'time.cloudflare.com:2095#优质资源',
     'shopify.com:2095#优质资源',
     'time.is:2095#优质资源',
     'icook.hk:2095#优质资源',
@@ -55,21 +69,6 @@ let addresses = [
     'edtunnel-dgp.pages.dev:2095#优质资源',
     'www.d-555.com:2095#优质资源',
     'fbi.gov:2095#优质资源',
-
-];
-
-// 设置优选地址api接口
-let addressesapi = [
-	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesapi.txt', //可参考内容格式 自行搭建。
-	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesipv6api.txt', //IPv6优选内容格式 自行搭建。
-	'https://wenben-ehq.pages.dev/IP.txt?token=4352934',
-];
-
-// 设置优选地址，不带端口号默认80，noTLS订阅生成
-let addressesnotls = [
-	//'www.visa.com.sg#资源优选域名',
-	//'www.wto.org:8080#官方优选域名',
-	//'www.who.int:8880#官方优选域名',
 ];
 
 // 设置优选noTLS地址api接口
